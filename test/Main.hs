a8346a9aa80509fbfb7a778d33fe8304cd8fd065
ekmett <ekmett@gmail.com>
2010-04-03 20:59:03 -0700

    added missing test suite to repository
    Ignore-this: bbeb272f0812b7a52ae59c361354995c
    
    darcs-hash:20100404035903-7b3c0-d2b9a73b57ab71a78fc71f660d42fb12e53598fe.gz

---
+++ test/Main.hs
@@ -0,0 +1,49 @@
+module Main where
+
+import Prelude hiding ((||),(&&)) 
+
+import Test.Framework (Test)
+import Test.Framework (defaultMain, testGroup)
+import Test.Framework.Providers.HUnit
+-- import Test.Framework.Providers.QuickCheck (testProperty)
+
+-- import Test.QuickCheck hiding ((==>))
+import Test.HUnit hiding (Test)
+
+-- import Control.Monad.ST
+
+-- import Data.List
+import Data.Logic.Ersatz
+
+main :: IO () 
+main = defaultMain tests
+
+ignore :: Functor f => f a -> f () 
+ignore = fmap (const ())
+
+tests :: [Test]
+tests = 
+    [ testGroup "sat" $ zipWith (testCase . show) [1 :: Int ..] $
+        [ showSAT (return ()) @?= "p cnf 0 0\n"
+        , showSAT (ignore litExists) @?= "p cnf 1 0\n"
+        , showSAT (ignore litForall) @?= "p cnf 2 0\na 1\n"
+        , showSAT (do x <- forall; y <- exists; assertLits [x,y]) @?= "p cnf 2 1\na 1\ne 2\n1 2 0\n"
+        ] 
+    ]
+
+full_adder :: Bit b -> Bit b -> Bit b -> (Bit b, Bit b)
+full_adder a b cin = (s2, c1 || c2)
+        where (s1,c1) = half_adder a b
+              (s2,c2) = half_adder s1 cin
+
+half_adder :: Bit b -> Bit b -> (Bit b, Bit b)
+half_adder a b = (a `xor` b, a && b)
+
+{-
+currying_works :: SAT s ()
+currying_works = do
+        x <- forall
+        y <- forall
+        z <- forall
+        assert $ (x && y) ==> z === x ==> y ==> z 
+-}
