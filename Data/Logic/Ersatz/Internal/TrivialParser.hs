9d9b4b2b639732fbb13f602d5bc2a3243d723d6c
Johan Kiviniemi <devel@johan.kiviniemi.name>
2013-03-06 02:10:26 +0200

    Support running (crypto)minisat and parsing the solution

---
+++ Data/Logic/Ersatz/Internal/TrivialParser.hs
@@ -0,0 +1,53 @@
+-- | A trivial, inefficient parser with no support for error reporting.
+module Data.Logic.Ersatz.Internal.TrivialParser
+  ( Parser
+  , runParser
+  , sepBy, sepBy1
+  , token, string
+  , integer, natural
+  , eof
+  , satisfy
+  ) where
+
+import Control.Applicative
+import Control.Monad.State
+import Data.Char (isDigit)
+import Data.Traversable (traverse)
+
+type Parser t a = StateT [t] [] a
+
+runParser :: Parser t a -> [t] -> [a]
+runParser = evalStateT
+
+sepBy :: Parser t a -> Parser t sep -> Parser t [a]
+sepBy p sep = sepBy1 p sep <|> pure []
+
+sepBy1 :: Parser t a -> Parser t sep -> Parser t [a]
+sepBy1 p sep = (:) <$> p <*> many (sep *> p)
+
+token :: Eq t => t -> Parser t t
+token t = satisfy (== t)
+
+string :: Eq t => [t] -> Parser t [t]
+string = traverse token
+
+integer :: (Integral i, Read i) => Parser Char i
+integer = negation <*> natural
+
+negation :: Num n => Parser Char (n -> n)
+negation  =  negate <$ token '-'
+         <|> pure id
+
+natural :: (Integral i, Read i) => Parser Char i
+natural = read <$> some (satisfy isDigit)
+
+eof :: Parser t ()
+eof = do
+  [] <- get
+  return ()
+
+satisfy :: (t -> Bool) -> Parser t t
+satisfy f = do
+  (t:ts) <- get
+  guard (f t)
+  t <$ put ts
---
+++ Data/Logic/Ersatz/Solver/Minisat.hs
@@ -0,0 +1,70 @@
+module Data.Logic.Ersatz.Solver.Minisat
+  ( minisat
+  , cryptominisat
+  , minisatPath
+  ) where
+
+import Control.Applicative
+import Control.Exception (IOException, handle)
+import qualified Data.IntMap as IntMap
+import System.Exit (ExitCode(..))
+import System.IO.Temp (withSystemTempDirectory)
+import System.Process (readProcessWithExitCode)
+
+import Data.Logic.Ersatz.Internal.Problem (qdimacs)
+import Data.Logic.Ersatz.Internal.TrivialParser
+import Data.Logic.Ersatz.Solution
+
+minisat :: Solver IO
+minisat = minisatPath "minisat"
+
+cryptominisat :: Solver IO
+cryptominisat = minisatPath "cryptominisat"
+
+minisatPath :: FilePath -> Solver IO
+minisatPath path qbf =
+  withSystemTempDirectory "ersatz" $ \dir -> do
+    let problemPath  = dir ++ "/problem.cnf"
+        solutionPath = dir ++ "/solution"
+
+    writeFile problemPath (qdimacs qbf)
+
+    (exit, _out, _err) <-
+      readProcessWithExitCode path [problemPath, solutionPath] []
+
+    sol <- parseSolutionFile solutionPath
+
+    return (resultOf exit, sol)
+
+resultOf :: ExitCode -> Result
+resultOf (ExitFailure 10) = Satisfied
+resultOf (ExitFailure 20) = Unsatisfied
+resultOf _                = Unsolved
+
+parseSolutionFile :: FilePath -> IO Solution
+parseSolutionFile path = handle handler $ do
+  parseSolution <$> readFile path
+  where
+    handler :: IOException -> IO Solution
+    handler _ = return IntMap.empty
+
+parseSolution :: String -> Solution
+parseSolution input =
+  case runParser solution input of
+    (s:_) -> s
+    _     -> IntMap.empty
+
+solution :: Parser Char Solution
+solution = do
+  _ <- string "SAT\n"
+  IntMap.fromList <$> values
+
+values :: Parser Char [(Int, Bool)]
+values  = (value `sepBy` token ' ')
+       <* (() <$ token '\n' <|> eof)
+
+value :: Parser Char (Int, Bool)
+value = toPair <$> integer
+  where
+    toPair n | n >= 0    = ( n, True)
+             | otherwise = (-n, False)
