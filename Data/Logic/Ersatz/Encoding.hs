078f116def152e8f09742142ab805373e7bebbf6
Johan Kiviniemi <devel@johan.kiviniemi.name>
2013-03-08 09:58:28 +0200

    Encoding: Add instances: HashMap, IntMap, Map, Seq, Tree

--- Data/Logic/Ersatz/Encoding.hs
+++ Data/Logic/Ersatz/Encoding.hs
@@ -6,7 +6,12 @@ module Data.Logic.Ersatz.Encoding
 
 import Control.Applicative
 import Data.Array
+import Data.HashMap.Lazy (HashMap)
+import Data.IntMap (IntMap)
+import Data.Map (Map)
+import Data.Sequence (Seq)
 import Data.Traversable (Traversable, sequenceA, traverse)
+import Data.Tree (Tree)
 
 import Data.Logic.Ersatz.Internal.Problem
 import Data.Logic.Ersatz.Solution
@@ -62,21 +67,41 @@ instance (Encoding a, Encoding b, Encoding c, Encoding d, Encoding e, Encoding f
   decode s (a,b,c,d,e,f,g,h) = go <$> decode s a <*> decode s b <*> decode s c <*> decode s d <*> decode s e <*> decode s f <*> decode s g <*> decode s h
     where go a' b' c' d' e' f' g' h' = (,,,,,,,) <$> a' <*> b' <*> c' <*> d' <*> e' <*> f' <*> g' <*> h'
 
+instance Encoding a => Encoding [a] where
+  type Decoded [a] = [Decoded a]
+  decode = decodeTraversable
+
+instance (Ix i, Encoding e) => Encoding (Array i e) where
+  type Decoded (Array i e) = Array i (Decoded e)
+  decode = decodeTraversable
+
 instance (Encoding a, Encoding b) => Encoding (Either a b) where
   type Decoded (Either a b) = Either (Decoded a) (Decoded b)
   decode s (Left  a) = fmap Left  <$> decode s a
   decode s (Right b) = fmap Right <$> decode s b
 
-instance Encoding a => Encoding [a] where
-  type Decoded [a] = [Decoded a]
+instance Encoding a => Encoding (HashMap k a) where
+  type Decoded (HashMap k a) = HashMap k (Decoded a)
+  decode = decodeTraversable
+
+instance Encoding a => Encoding (IntMap a) where
+  type Decoded (IntMap a) = IntMap (Decoded a)
+  decode = decodeTraversable
+
+instance Encoding a => Encoding (Map k a) where
+  type Decoded (Map k a) = Map k (Decoded a)
   decode = decodeTraversable
 
 instance Encoding a => Encoding (Maybe a) where
   type Decoded (Maybe a) = Maybe (Decoded a)
   decode = decodeTraversable
 
-instance (Ix i, Encoding e) => Encoding (Array i e) where
-  type Decoded (Array i e) = Array i (Decoded e)
+instance Encoding a => Encoding (Seq a) where
+  type Decoded (Seq a) = Seq (Decoded a)
+  decode = decodeTraversable
+
+instance Encoding a => Encoding (Tree a) where
+  type Decoded (Tree a) = Tree (Decoded a)
   decode = decodeTraversable
 
 decodeTraversable :: (Traversable f, Encoding a) => Solution -> f a -> IO (Maybe (f (Decoded a)))
