8c451c6d1e198717315673365a93e88405901904
Edward Kmett <ekmett@gmail.com>
2013-03-09 03:36:17 -0500

    Generic Variable instances

--- src/Ersatz/Variable.hs
+++ src/Ersatz/Variable.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DefaultSignatures, TypeOperators, FlexibleContexts, UndecidableInstances #-}
 --------------------------------------------------------------------
 -- |
 -- Copyright :  (c) Edward Kmett 2010-2013, Johan Kiviniemi 2013
@@ -14,10 +15,36 @@ module Ersatz.Variable
 import Control.Applicative
 import Ersatz.Internal.Literal
 import Ersatz.Monad
+import GHC.Generics
+
+class GVariable f where
+  gexists :: MonadSAT m => m (f a)
+  gforall :: MonadSAT m => m (f a)
+
+instance GVariable U1 where
+  gexists = return U1
+  gforall = return U1
+
+instance (GVariable f, GVariable g) => GVariable (f :*: g) where
+  gexists = (:*:) <$> gexists <*> gexists
+  gforall = (:*:) <$> gforall <*> gforall
+
+instance Variable a => GVariable (K1 i a) where
+  gexists = K1 <$> exists
+  gforall = K1 <$> forall
+
+instance GVariable f => GVariable (M1 i c f) where
+  gexists = M1 <$> gexists
+  gforall = M1 <$> gforall
 
 class Variable t where
   exists :: MonadSAT m => m t
+  default exists :: (MonadSAT m, Generic t, GVariable (Rep t)) => m t
+  exists = to <$> gexists
+
   forall :: MonadSAT m => m t
+  default forall :: (MonadSAT m, Generic t, GVariable (Rep t)) => m t
+  forall = to <$> gforall
 
 instance Variable Lit where
   exists = Lit <$> exists
@@ -27,31 +54,9 @@ instance Variable Literal where
   exists = literalExists
   forall = literalForall
 
-instance (Variable a, Variable b) => Variable (a,b) where
-  exists = (,) <$> exists <*> exists
-  forall = (,) <$> forall <*> forall
-
-instance (Variable a, Variable b, Variable c) => Variable (a,b,c) where
-  exists = (,,) <$> exists <*> exists <*> exists
-  forall = (,,) <$> forall <*> forall <*> forall
-
-instance (Variable a, Variable b, Variable c, Variable d) => Variable (a,b,c,d) where
-  exists = (,,,) <$> exists <*> exists <*> exists <*> exists
-  forall = (,,,) <$> forall <*> forall <*> forall <*> forall
-
-instance (Variable a, Variable b, Variable c, Variable d, Variable e) => Variable (a,b,c,d,e) where
-  exists = (,,,,) <$> exists <*> exists <*> exists <*> exists <*> exists
-  forall = (,,,,) <$> forall <*> forall <*> forall <*> forall <*> forall
-
-instance (Variable a, Variable b, Variable c, Variable d, Variable e, Variable f) => Variable (a,b,c,d,e,f) where
-  exists = (,,,,,) <$> exists <*> exists <*> exists <*> exists <*> exists <*> exists
-  forall = (,,,,,) <$> forall <*> forall <*> forall <*> forall <*> forall <*> forall
-
-instance (Variable a, Variable b, Variable c, Variable d, Variable e, Variable f, Variable g) => Variable (a,b,c,d,e,f,g) where
-  exists = (,,,,,,) <$> exists <*> exists <*> exists <*> exists <*> exists <*> exists <*> exists
-  forall = (,,,,,,) <$> forall <*> forall <*> forall <*> forall <*> forall <*> forall <*> forall
-
-instance (Variable a, Variable b, Variable c, Variable d, Variable e, Variable f, Variable g, Variable h) => Variable (a,b,c,d,e,f,g,h) where
-  exists = (,,,,,,,) <$> exists <*> exists <*> exists <*> exists <*> exists <*> exists <*> exists <*> exists
-  forall = (,,,,,,,) <$> forall <*> forall <*> forall <*> forall <*> forall <*> forall <*> forall <*> forall
-
+instance (Variable a, Variable b) => Variable (a,b)
+instance (Variable a, Variable b, Variable c) => Variable (a,b,c)
+instance (Variable a, Variable b, Variable c, Variable d) => Variable (a,b,c,d)
+instance (Variable a, Variable b, Variable c, Variable d, Variable e) => Variable (a,b,c,d,e)
+instance (Variable a, Variable b, Variable c, Variable d, Variable e, Variable f) => Variable (a,b,c,d,e,f)
+instance (Variable a, Variable b, Variable c, Variable d, Variable e, Variable f, Variable g) => Variable (a,b,c,d,e,f,g)
