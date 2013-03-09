3f3a5d47b6e8436d6d771c8059e69f50162b74bc
Edward Kmett <ekmett@gmail.com>
2013-03-09 01:51:22 -0500

    Added missing module headers

--- src/Ersatz.hs
+++ src/Ersatz.hs
@@ -1,15 +1,28 @@
+--------------------------------------------------------------------
+-- |
+-- Copyright :  (c) Edward Kmett 2010-2013, Johan Kiviniemi 2013
+-- License   :  BSD3
+-- Maintainer:  Edward Kmett <ekmett@gmail.com>
+-- Stability :  experimental
+-- Portability: non-portable
+--
+--------------------------------------------------------------------
 module Ersatz
   ( module Ersatz.Bit
   , module Ersatz.Bits
   , module Ersatz.Encoding
   , module Ersatz.Monad
+  , module Ersatz.Problem
   , module Ersatz.Solution
   , module Ersatz.Solver
+  , module Ersatz.Variable
   ) where
 
 import Ersatz.Bit
 import Ersatz.Bits
 import Ersatz.Encoding
 import Ersatz.Monad
+import Ersatz.Problem
 import Ersatz.Solution
 import Ersatz.Solver
+import Ersatz.Variable
