c25933eb6a5513578ed42d805d0ffad30d207092
cutsea110 <cutsea110@gmail.com>
2016-10-02 07:15:56 +0900

    support cryptominisat5

--- src/Ersatz/Solver/Minisat.hs
+++ src/Ersatz/Solver/Minisat.hs
@@ -30,13 +30,17 @@ import System.Process (readProcessWithExitCode)
 import qualified Data.ByteString.Char8 as B
 import Data.List ( foldl' )
 
+data SolutionFormat = Minisat
+                    | SATCompetition
+                    deriving (Show, Eq)
+
 -- | 'Solver' for 'SAT' problems that tries to invoke the @minisat@ executable from the @PATH@
 minisat :: MonadIO m => Solver SAT m
 minisat = minisatPath "minisat"
 
 -- | 'Solver' for 'SAT' problems that tries to invoke the @cryptominisat@ executable from the @PATH@
 cryptominisat :: MonadIO m => Solver SAT m
-cryptominisat = minisatPath "cryptominisat"
+cryptominisat = cryptoMinisatPath "cryptominisat5"
 
 -- | 'Solver' for 'SAT' problems that tries to invoke a program that takes @minisat@ compatible arguments.
 --
@@ -54,17 +58,41 @@ minisatPath path problem = liftIO $
 
     return (resultOf exit, sol)
 
+-- The 'FilePath' refers to the path to the executable.
+cryptoMinisatPath :: MonadIO m => FilePath -> Solver SAT m
+cryptoMinisatPath path problem = liftIO $
+  withTempFiles ".cnf" "" $ \problemPath dratTrimPath -> do
+    withFile problemPath WriteMode $ \fh ->
+      hPutBuilder fh (dimacs problem)
+
+    (exit, solutionStr, _err) <-
+      readProcessWithExitCode path [problemPath, dratTrimPath] []
+
+    let sol = parseSolution SATCompetition $ B.pack solutionStr
+
+    return (resultOf exit, sol)
+
 parseSolutionFile :: FilePath -> IO (IntMap Bool)
-parseSolutionFile path = handle handler (parseSolution <$> B.readFile path)
+parseSolutionFile path = handle handler (parseSolution Minisat <$> B.readFile path)
   where
     handler :: IOException -> IO (IntMap Bool)
     handler _ = return IntMap.empty
 
-parseSolution :: B.ByteString -> IntMap Bool
-parseSolution s =
+parseSolution :: SolutionFormat -> B.ByteString -> IntMap Bool
+parseSolution Minisat s =
   case B.words s of
     x : ys | x == "SAT" ->
           foldl' ( \ m y -> let Just (v,_) = B.readInt y
                             in  if 0 == v then m else IntMap.insert (abs v) (v>0) m
                  ) IntMap.empty ys
     _ -> IntMap.empty -- WRONG (should be Nothing)
+parseSolution SATCompetition s = go [] $ B.lines s
+  where
+    go xs (y:ys)
+      | "c " `B.isPrefixOf` y = go xs ys
+      | y == "s SATISFIABLE" = go xs ys
+      | "v " `B.isPrefixOf` y = go (B.drop 2 y:xs) ys
+      | otherwise = IntMap.empty -- WRONG (should be Nothing)
+    go xs [] = foldl' ( \ m y -> let Just (v,_) = B.readInt y
+                                 in if 0 == v then m else IntMap.insert (abs v) (v>0) m
+                      ) IntMap.empty $ concatMap B.words $ reverse xs
