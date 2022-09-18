5c3ebd2423c4c98bbab5c97f850eac851dce9170
Johannes Waldmann <waldmann@denk>
2022-09-18 13:51:48 +0200

    add haddocks for Ersatz.Relation (contributed by A. Swiridoff)

--- src/Ersatz/Relation/Data.hs
+++ src/Ersatz/Relation/Data.hs
@@ -1,13 +1,20 @@
 {-# language TypeFamilies #-}
 
-module Ersatz.Relation.Data ( Relation
+module Ersatz.Relation.Data ( 
+-- * The 'Relation' type
+  Relation
+-- * Construction
 , relation, symmetric_relation
 , build
 , buildFrom
 , identity
+-- * Components
 , bounds, (!), indices, assocs, elems
+-- *
 , table
 )  where
+
+import Prelude hiding ( and )
 
 import Ersatz.Bit
 import Ersatz.Codec
@@ -18,6 +25,15 @@ import Control.Monad (guard)
 import qualified Data.Array as A
 import Data.Array ( Array, Ix )
 
+
+-- | Here, a 'Relation' is thought of as a binary relation \(R \subseteq A \times B \)
+-- with domain \(A\) and codomain \(B\). All \( x \in A \) are represented by values in @a@
+-- and all \( y \in B \) by values in @b@.
+-- Since a relation is stored internally as a matrix, i.e., as an 'Array' of 'Bit'-values, 
+-- @a@ and @b@ have to be instances of 'Ix'.
+--
+-- It can also be thought of a 'Relation', for example, as an abstract rewriting system
+-- or a directed graph.
 newtype Relation a b = Relation (A.Array (a, b) Bit)
 
 instance (Ix a, Ix b) => Codec (Relation a b) where
@@ -25,8 +41,15 @@ instance (Ix a, Ix b) => Codec (Relation a b) where
   decode s (Relation a) = decode s a
   encode a = Relation $ encode a
 
-relation :: ( Ix a, Ix b, MonadSAT s m )
-         => ((a,b),(a,b)) -> m ( Relation a b )
+
+-- | Constructs an indeterminate relation \( R \subseteq A \times B \).
+relation :: ( Ix a, Ix b, MonadSAT s m ) =>
+  ((a,b),(a,b)) -- ^ A tuple of bounds, which are the lowest and highest indices in the array, 
+                -- that correspond to the matrix representation of \(R\).
+                -- Each index within the bounds represents an element \( (x,y) \in A \times B \) 
+                -- that may be contained in \(R\).
+  -> m ( Relation a b ) -- ^ An indeterminate relation \(R\) with a fixed domain \(A\) and codomain \(B\). 
+                        -- The relation carries a SAT state and can be evaluated by a 'Ersatz.Solution.Solver'.
 relation bnd = do
     pairs <- sequence $ do
         p <- A.range bnd
@@ -35,9 +58,15 @@ relation bnd = do
             return ( p, x )
     return $ build bnd pairs
 
+-- | Constructs an indeterminate relation \( R \subseteq A \times A \) with the constraint, 
+-- that it is symmetric, i.e., \( \forall x, y \in A: ((x,y) \in R) \rightarrow ((y,x) \in R) \).
+--
+-- It can be thought of a symmetric relation as an undirected graph.
 symmetric_relation ::
   (MonadSAT s m, Ix b) =>
-  ((b, b), (b, b)) -> m (Relation b b)
+  ((b, b), (b, b)) -- ^ Since a symmetric relation must be homogeneous, the domain must equals the codomain, 
+                   -- hence for the bounds @((p,q),(r,s))@ must hold @p=q@ and @r=s@.
+  -> m (Relation b b)
 symmetric_relation bnd = do
     pairs <- sequence $ do
         (p,q) <- A.range bnd
@@ -48,38 +77,98 @@ symmetric_relation bnd = do
                    : [ ((q,p), x) | p /= q ]
     return $ build bnd $ concat pairs
 
+-- | Constructs a relation \(R \subseteq A \times B \) from a list.
+-- 
+-- ==== __Example__
+--
+-- @
+-- r = build ((0,'a'),(1,'b')) [((0,'a'), true), ((0,'b'), false), 
+--                          ((1,'a'), false), ((1,'b'), true))]
+-- @
 build :: ( Ix a, Ix b )
       => ((a,b),(a,b))
-      -> [ ((a,b), Bit ) ]
+      -> [ ((a,b), Bit ) ] -- ^ A list of tuples, where the first element represents an element
+                           -- \( (x,y) \in A \times B \) and the second element is a positive 'Bit',
+                           -- if \( (x,y) \in R \) or a negative 'Bit', if \( (x,y) \notin R \).
       -> Relation a b
 build bnd pairs = Relation $ A.array bnd pairs
 
+-- | Constructs a relation \(R \subseteq A \times B \) from a function.
 buildFrom :: (Ix a, Ix b)
-          => (a -> b -> Bit) -> ((a,b),(a,b)) -> Relation a b
-buildFrom p bnd = build bnd $ flip map (A.range bnd) $ \ (i,j) ->
-    ((i,j), p i j)
+          => (a -> b -> Bit) -- ^ A function with the specified signature, that assigns a 'Bit'-value 
+                             -- to each element \( (x,y) \in A \times B \).
+          -> ((a,b),(a,b))
+          -> Relation a b
+buildFrom p bnd = build bnd $ flip map (A.range bnd) $ \ (i,j) -> ((i, j), p i j)
 
+-- | Constructs the identity relation \(I \subseteq A \times A, I = \{ (x,x) ~|~ x \in A \} \).
 identity :: (Ix a)
-         => ((a,a),(a,a)) -> Relation a a
+         => ((a,a),(a,a)) -- ^ Since the identity relation is homogeneous, the domain must equals the codomain, 
+                          -- hence for the bounds @((p,q),(r,s))@ must hold @p=q@ and @r=s@.
+         -> Relation a a
 identity = buildFrom (\ i j -> bool $ i == j)
 
 
+-- | The bounds of the array, that correspond to the matrix representation of the given relation.
+--
+-- ==== __Example__
+--
+-- >>> r = build ((0,0),(1,1)) [((0,0), false), ((0,1), true), ((1,0), true), ((1,1), false))]
+-- >>> bounds r
+-- ((0,0),(1,1))
 bounds :: (Ix a, Ix b) => Relation a b -> ((a,b),(a,b))
 bounds ( Relation r ) = A.bounds r
 
+-- | The list of indices, where each index represents an element \( (x,y) \in A \times B \) 
+-- that may be contained in the given relation \(R \subseteq A \times B \).
+--
+-- ==== __Example__
+--
+-- >>> r = build ((0,0),(1,1)) [((0,0), false), ((0,1), true), ((1,0), true), ((1,1), false))]
+-- >>> indices r
+-- [(0,0),(0,1),(1,0),(1,1)]
 indices :: (Ix a, Ix b) => Relation a b -> [(a, b)]
 indices ( Relation r ) = A.indices r
 
+-- | The list of tuples for the given relation \(R \subseteq A \times B \), 
+-- where the first element represents an element \( (x,y) \in A \times B \) 
+-- and the second element indicates via a 'Bit' , if \( (x,y) \in R \) or not.
+-- 
+-- ==== __Example__
+--
+-- >>> r = build ((0,0),(1,1)) [((0,0), false), ((0,1), true), ((1,0), true), ((1,1), false))]
+-- >>> assocs r
+-- [((0,0),Var (-1)),((0,1),Var 1),((1,0),Var 1),((1,1),Var (-1))]
 assocs :: (Ix a, Ix b) => Relation a b -> [((a, b), Bit)]
 assocs ( Relation r ) = A.assocs r
 
+-- | The list of elements of the array,
+-- that correspont to the matrix representation of the given relation.
+--
+-- ==== __Example__
+--
+-- >>> r = build ((0,0),(1,1)) [((0,0), false), ((0,1), true), ((1,0), true), ((1,1), false))]
+-- >>> elems r
+-- [Var (-1),Var 1,Var 1,Var (-1)]
 elems :: (Ix a, Ix b) => Relation a b -> [Bit]
 elems ( Relation r ) = A.elems r
 
+-- | The 'Bit'-value for a given element \( (x,y) \in A \times B \) 
+-- and a given relation \(R \subseteq A \times B \) that indicates,
+-- if \( (x,y) \in R \) or not.
+-- 
+-- ==== __Example__
+--
+-- >>> r = build ((0,0),(1,1)) [((0,0), false), ((0,1), true), ((1,0), true), ((1,1), false))]
+-- >>> r ! (0,0)
+-- Var (-1)
+-- >>> r ! (0,1)
+-- Var 1
 (!) :: (Ix a, Ix b) => Relation a b -> (a, b) -> Bit
 Relation r ! p = r A.! p
 
-
+-- | To print a satisfying assignment from a SAT solver, where the assignment is interpreted as a relation.
+-- @putStrLn $ table \</assignment/\>@ corresponds to the matrix representation of this relation.
 table :: (Enum a, Ix a, Enum b, Ix b)
       => Array (a,b) Bool -> String
 table r = unlines $ do
--- src/Ersatz/Relation/Op.hs
+++ src/Ersatz/Relation/Op.hs
@@ -2,7 +2,9 @@
 
 module Ersatz.Relation.Op
 
-( mirror
+( 
+-- * Operations
+  mirror
 , union
 , complement
 , difference
@@ -21,26 +23,45 @@ import Ersatz.Bit (and, or, not)
 
 import Data.Ix
 
+-- | Constructs the converse relation \( R^{-1} \subseteq B \times A \) of a relation 
+-- \( R \subseteq A \times B \), which is defined by \( R^{-1} = \{ (y,x) ~|~ (x,y) \in R \} \).
 mirror :: ( Ix a , Ix b ) => Relation a b -> Relation b a
 mirror r =
     let ((a,b),(c,d)) = bounds r
     in  build ((b,a),(d,c)) $ do (x,y) <- indices r ; return ((y,x), r!(x,y))
 
+-- | Constructs the complement relation \( \overline{R} \) 
+-- of a relation \( R \subseteq A \times B \), which is defined by 
+-- \( \overline{R}  = \{ (x,y) \in A \times B ~|~ (x,y) \notin R \} \).
 complement :: ( Ix a , Ix b ) => Relation a b -> Relation a b
 complement r =
     build (bounds r) $ do i <- indices r ; return ( i, not $ r!i )
 
+-- | Constructs the difference \( R \setminus S \) of the relations 
+-- \(R\) and \(S\), that contains all elements that are in \(R\) but not in \(S\), i.e.,
+-- \( R \setminus S = \{ (x,y) \in R ~|~ (x,y) \notin S \} \).
+--
+-- Note that if \( R \subseteq A \times B \) and \( S \subseteq C \times D \),
+-- then \( A \times B \) must be a subset of \( C \times D \) and
+-- \( R \setminus S \subseteq A \times B \).
 difference :: ( Ix a , Ix b )
         => Relation a b -> Relation a b ->  Relation a b
 difference r s =
     intersection r $ complement s
 
+-- | Constructs the union \( R \cup S \) of the relations \( R \) and \( S \).
+--
+-- Note that for \( R \subseteq A \times B \) and \( S \subseteq C \times D \)
+-- must hold \( A \times B \subseteq C \times D \).
 union :: ( Ix a , Ix b )
         => Relation a b -> Relation a b ->  Relation a b
 union r s =  build ( bounds r ) $ do
     i <- indices r
     return (i, or [ r!i, s!i ] )
 
+-- | Constructs the composition \( R \cdot S \) of the relations 
+-- \( R \subseteq A \times B \) and \( S \subseteq B \times C \), which is 
+-- defined by \( R \cdot S = \{ (a,c) ~|~ ((a,b) \in R) \land ((b,c) \in S) \} \).
 product :: ( Ix a , Ix b, Ix c )
         => Relation a b -> Relation b c ->  Relation a c
 product a b =
@@ -54,8 +75,13 @@ product a b =
                 return $ and [ a!(x,y), b!(y,z) ]
                 )
 
+-- | Constructs the relation \( R^{n} \) that results, if a relation
+-- \( R \subseteq A \times A \) is composed \(n\) times with itself.
+--
+-- \( R^{0} \) is the identity relation \( I_{R} = \{ (x,x) ~|~ x \in A \} \) of \(R\).
 power  :: ( Ix a  )
-        => Int -> Relation a a -> Relation a a
+        => Int -- ^ \(n\)
+        -> Relation a a -> Relation a a
 power 0 r = identity ( bounds r )
 power 1 r = r
 power e r =
@@ -66,6 +92,10 @@ power e r =
         0 -> s2
         _ -> product s2 r
 
+-- | Constructs the intersection \( R \cap S \) of the relations \( R \) and \( S \).
+--
+-- Note that for \( R \subseteq A \times B \) and \( S \subseteq C \times D \)
+-- must hold \( A \times B \subseteq C \times D \).
 intersection :: ( Ix a , Ix b)
       => Relation a b -> Relation a b
       -> Relation a b
@@ -73,10 +103,16 @@ intersection r s = build ( bounds r ) $ do
         i <- indices r
         return (i, and [ r!i, s!i ] )
 
+-- | Constructs the reflexive closure \( R \cup I_{R} \) of the relation 
+-- \( R \subseteq A \times A \), where \( I_{R} = \{ (x,x) ~|~ x \in A \} \) 
+-- is the identity relation of \(R\).
 reflexive_closure :: Ix a => Relation a a -> Relation a a
 reflexive_closure t =
     union t $ identity $ bounds t
 
+-- | Constructs the symmetric closure \( R \cup R^{-1} \) of the relation 
+-- \( R \subseteq A \times A \), where \( R^{-1} = \{ (y,x) ~|~ (x,y) \in R \} \)
+-- is converse relation of \(R\).
 symmetric_closure :: Ix a => Relation a a -> Relation a a
 symmetric_closure r =
     union r $ mirror r
