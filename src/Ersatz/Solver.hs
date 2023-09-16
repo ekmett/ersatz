136916a7831c3f6ad5907ec1a49069271fa40267
Ryan Scott <ryan.gl.scott@gmail.com>
2023-09-16 06:45:43 -0400

    Provide Haddocks for solveWith
    The exact meaning of what `solveWith` returns is not obvious at a first glance.
    This patch provides some Haddocks for `solveWith` that better explains this and
    provides a complete example of how to use `solveWith`.

--- src/Ersatz/Solver.hs
+++ src/Ersatz/Solver.hs
@@ -23,6 +23,36 @@ import Ersatz.Solver.DepQBF
 import Ersatz.Solver.Minisat
 import Ersatz.Solver.Z3
 
+-- | @'solveWith' solver prob@ solves a SAT problem @prob@ with the given
+-- @solver@. It returns a pair consisting of:
+--
+-- 1. A 'Result' that indicates if @prob@ is satisfiable ('Satisfied'),
+--    unsatisfiable ('Unsatisfied'), or if the solver could not determine any
+--    results ('Unsolved').
+--
+-- 2. A 'Decoded' answer that was decoded using the solution to @prob@. Note
+--    that this answer is only meaningful if the 'Result' is 'Satisfied' and
+--    the answer value is in a 'Just'.
+--
+-- Here is a small example of how to use 'solveWith':
+--
+-- @
+-- import Ersatz
+--
+-- main :: IO ()
+-- main = do
+--   res <- 'solveWith' minisat $ do
+--     (b1 :: Bit) <- exists
+--     (b2 :: Bit) <- exists
+--     assert (b1 === b2)
+--     pure [b1, b2]
+--   case res of
+--     (Satisfied, Just answer) -> print answer
+--     _ -> fail "Could not solve problem"
+-- @
+--
+-- Depending on the whims of @minisat@, this program may print either
+-- @[False, False]@ or @[True, True]@.
 solveWith ::
   (Monad m, HasSAT s, Default s, Codec a) =>
   Solver s m -> StateT s m a -> m (Result, Maybe (Decoded a))
